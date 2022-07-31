d46123d2ef8ce9a3d1cfc68ad19cdc46fdd967d0
adamyordan01 <adamyordan0102@gmail.com>
2022-07-31 09:58:40 +0700

    Renew checking ticket

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\TransactionDetail;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CheckController extends Controller
@@ -14,17 +15,36 @@ class CheckController extends Controller
 
     public function show($id)
     {
-        $detail = TransactionDetail::where('barcode', $id)->first();
+        $detail = TransactionDetail::with('product')->where('barcode', $id)->first();
+        // dd($detail);
+        // $today = date('Y-m-d');
+        // $date_event = Carbon::parse($detail->product->date_event)->format('Y-m-d');
+        
 
         if ($detail) {
-            $status = $detail->status;
-            $detail->update([
-                'status' => 1
-            ]);
-        } else {
-            $status = 2;
-        }
+            $today = date('Y-m-d');
+            $date_event = Carbon::parse($detail->product->date_event)->format('Y-m-d');
+            if ($today == $date_event) {
+                // check if date_event is today
+                // dd('date_event is today');
+                $status = 1;
+                return view('check.show', compact('detail', 'status'));
+            } else if ($today < $date_event) {
+                // check if date_event is in the future
+                // dd('date_event is in the future');
+                $status = 2;
+                return view('check.show', compact('detail', 'status'));
+            } else if ($today > $date_event) {
+                // check if date_event is in the past
+                // dd('date_event is in the past');
+                $status = 3;
+                return view('check.show', compact('detail', 'status'));
+            }
 
-        return view('check.show', compact('status'));
+        } else {
+            // dd('barcode not found');
+            $status = 4;
+            return view('check.show', compact('status'));
+        }
     }
 }
--- resources/views/check/show.blade.php
+++ resources/views/check/show.blade.php
@@ -1,18 +1,28 @@
-@if ($status == 0)
+@if ($status == 1)
     <div class="col-md-12">
         <div class="text-center text-success mb-4">
             <i class="fas fa-check-circle" style="font-size: 4em"></i>
         </div>
+        <h3 class="text-center">{{ $detail->product->name }}</h3>
         <h3 class="text-center text-success">TIKET BERHASIL DIVERIFIKASI</h3>
     </div>
-@elseif ($status == 1)
+@elseif ($status == 2)
     <div class="col-md-12">
         <div class="text-center text-warning mb-4">
-            <i class="fas fa-exclamation" style="font-size: 4em"></i>
+            <i class="fas fa-hourglass-end" style="font-size: 4em"></i>
         </div>
-        <h3 class="text-center text-warning">TIKET SUDAH DIGUNAKAN</h3>
+        <h3 class="text-center">{{ $detail->product->name }}</h3>
+        <h3 class="text-center text-warning">ACARA BELUM DIMULAI</h3>
     </div>
-@elseif ($status == 2)
+@elseif ($status == 3)
+    <div class="col-md-12">
+        <div class="text-center text-warning mb-4">
+            <i class="far fa-frown" style="font-size: 4em"></i>
+        </div>
+        <h3 class="text-center">{{ $detail->product->name }}</h3>
+        <h3 class="text-center text-warning">ACARA TELAH BERAKHIR</h3>
+    </div>
+@elseif ($status == 4)
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="text-center text-danger mb-4">
