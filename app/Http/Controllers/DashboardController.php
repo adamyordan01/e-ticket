b8a015d7853c5a0e86ce77429f5f2eaa7dc713e4
adamyordan01 <adamyordan0102@gmail.com>
2022-07-31 12:37:17 +0700

    Renew view dashboard by role

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,37 +4,94 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Models\Transaction;
+use App\Models\TransactionDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $income = Transaction::sum('grand_total');
-        $product = Product::count();
+        // $user = Auth::user()->role->name == 'petugas loket';
+        // dd($user);
+        if (auth()->user()->role->name == 'admin') {
+            $income = Transaction::sum('grand_total');
+            $product = Product::count();
+            $ticketSold = TransactionDetail::sum('quantity');
+            $transaction = Transaction::count();
+            return view('dashboard', [
+                'income' => $income,
+                'product' => $product,
+                'ticketSold' => $ticketSold,
+                'transaction' => $transaction,
+            ]);
+        } else if (auth()->user()->role->name == 'petugas loket') {
+            $income = Transaction::where('user_id', auth()->user()->id)->sum('grand_total');
+            $product = Product::count();
+            $ticketSold = TransactionDetail::where('user_id', auth()->user()->id)->sum('quantity');
+            $transaction = Transaction::where('user_id', auth()->user()->id)->count();
+            return view('dashboard', [
+                'income' => $income,
+                'product' => $product,
+                'ticketSold' => $ticketSold,
+                'transaction' => $transaction,
+            ]);
+        } else {
+            return view('dashboard');
+        }
         
-        return view('dashboard', [
-            'income' => $income,
-            'product' => $product,
-        ]);
     }
 
     public function getIncomeByDay()
     {
-        // get income by day
-        $incomeByDay = Transaction::selectRaw('DATE(created_at) as date, sum(grand_total) as total')
-            ->groupBy('date')
-            ->get();
-            // dd($incomeByDay);
-        $data = [];
+        if (auth()->user()->role->name == 'admin') {
+            $incomeByDay = Transaction::selectRaw('DATE(created_at) as date, sum(grand_total) as total')
+                ->groupBy('date')
+                ->get();
 
-        foreach ($incomeByDay as $row) {
-            $data['labels'][] = $row->date;
-            $data['income'][] = (int) $row->total;
+                $data = [];
+
+                foreach ($incomeByDay as $row) {
+                    $data['labels'][] = $row->date;
+                    $data['income'][] = (int) $row->total;
+                }
+
+                $data['chart_income_by_day'] = json_encode($data);
+
+                return $data;
+            // return response()->json($income);
+        } else if (auth()->user()->role->name == 'petugas loket') {
+            $incomeByDay = Transaction::where('user_id', auth()->user()->id)
+                ->selectRaw('DATE(created_at) as date, sum(grand_total) as total')
+                ->groupBy('date')
+                ->get();
+
+                $data = [];
+
+                foreach ($incomeByDay as $row) {
+                    $data['labels'][] = $row->date;
+                    $data['income'][] = (int) $row->total;
+                }
+
+                $data['chart_income_by_day'] = json_encode($data);
+
+                return $data;
+            // return response()->json($income);
         }
+        // get income by day
+        // $incomeByDay = Transaction::selectRaw('DATE(created_at) as date, sum(grand_total) as total')
+        //     ->groupBy('date')
+        //     ->get();
+        //     // dd($incomeByDay);
+        // $data = [];
 
-        $data['chart_income_by_day'] = json_encode($data);
+        // foreach ($incomeByDay as $row) {
+        //     $data['labels'][] = $row->date;
+        //     $data['income'][] = (int) $row->total;
+        // }
 
-        return $data;
+        // $data['chart_income_by_day'] = json_encode($data);
+
+        // return $data;
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.base', ["title" => "Dashboard"])
 
+@php
+    $role = auth()->user()->role->name;
+@endphp
+
 @push('style')
     <style>
         .btn-add {
@@ -32,62 +36,79 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-primary">
-                            {{-- <i class="fas fa-paper-plane"></i> --}}
-                            <i class="fas fa-dollar-sign"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Total Pendapatan</h4>
+    @if ($role == 'admin' || $role == 'petugas loket')
+        <div class="row">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                        <div class="card card-statistic-1">
+                            <div class="card-icon bg-success" style="background-color: #02dda5 !important">
+                                {{-- <i class="fas fa-paper-plane"></i> --}}
+                                <i class="fas fa-dollar-sign"></i>
                             </div>
-                            <div class="card-body">
-                                Rp.{{ number_format($income, 0, ',', '.') }}
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                    <h4>Total Pendapatan</h4>
+                                </div>
+                                <div class="card-body">
+                                    Rp.{{ number_format($income, 0, ',', '.') }}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-danger">
-                            <i class="fas fa-ticket-alt"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Total Produk</h4>
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                        <div class="card card-statistic-1">
+                            <div class="card-icon bg-primary">
+                                <i class="fas fa-ticket-alt"></i>
                             </div>
-                            <div class="card-body">
-                                {{ $product }}
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                    <h4>Jumlah Tiket Terjual</h4>
+                                </div>
+                                <div class="card-body">
+                                    {{ $ticketSold }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                        <div class="card card-statistic-1">
+                            <div class="card-icon bg-danger">
+                                <i class="fas fa-list"></i>
+                            </div>
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                    <h4>Total Transaksi</h4>
+                                </div>
+                                <div class="card-body">
+                                    {{ $transaction }}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-12 col-sm-12">
-          <div class="card">
-            <div class="card-header">
-              <h4>Statistics</h4>
-              {{-- <div class="card-header-action">
-                <div class="btn-group">
-                  <a href="#" class="btn btn-primary">Week</a>
-                  <a href="#" class="btn">Month</a>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-12 col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                <h4>Statistics</h4>
+                {{-- <div class="card-header-action">
+                    <div class="btn-group">
+                    <a href="#" class="btn btn-primary">Week</a>
+                    <a href="#" class="btn">Month</a>
+                    </div>
+                </div> --}}
                 </div>
-              </div> --}}
+                <div class="card-body">
+                <canvas id="myChart" height="182"></canvas>
+                </div>
             </div>
-            <div class="card-body">
-              <canvas id="myChart" height="182"></canvas>
             </div>
-          </div>
         </div>
-    </div>
+    @endif
 @endsection
 
 @push('script')
